a095b91e9c95016feb73f6992e5498481fc67ab6
sasakazu <kazuya.sasakura@gmail.com>
2021-09-10 19:01:54 +0900

    友達を探すを追加したところまで。

---
+++ movilog/search/searchFriends.swift
@@ -0,0 +1,23 @@
+//
+//  searchFriends.swift
+//  movilog
+//
+//  Created by 笹倉一也 on 2021/09/10.
+//
+
+import UIKit
+
+class searchFriends: UIViewController {
+
+    
+    @IBOutlet weak var searchFriendTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
